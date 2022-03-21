913ee9b5af2647d598ce8fd65e269b787fbf0d52
Afaed <justchoy99@gmail.com>
2022-03-20 22:16:38 -0700

    heroku commit

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,62 +1,95 @@
-//the ability to enter deposits offline
+// create variable to hold db connection
 let db;
 
-//create a new dv request for a budget dtabase
+// establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
+const request = indexedDB.open('budget_tracker', 1);
 
-const request = indexedDB.open("budget", 1);
-
+// this event will emit if the database version changes
 request.onupgradeneeded = function(event) {
-    const db =event.target.result;
-    db.createObjectStore("pending", { autoIncrement: true})
-}
 
+    // save a reference to the database
+    const db = event.target.result;
+
+    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+// upon a successful
 request.onsuccess = function(event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
 
+    // when db is successfully created with its object store or simply established a connection, save reference to db in global variable
     db = event.target.result;
-    //check if app is online and to check the database
+
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
     if (navigator.onLine) {
-        checkDatabase();
+        // todo: uploadTransaction();
     }
-}
+};
 
 request.onerror = function(event) {
-    //error log
-    console.log("Error: " + event.target.errorCode)
-}
+    // log error here
+    console.log(event.target.errorCode);
+};
 
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
-    const transaction =db.transaction(["pending"], "readWrite");
-    const store = transaction.objectStore("pending");
-   store.add(record)
+
+    // open a new transaction with the database with read and write permissions
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    // access the object store for `new_transaction`
+    const  budgetObjectStore = transaction.objectStore('new_transaction');
+
+    // add record to your store with add method
+    budgetObjectStore.add(record);
 }
 
-function checkDatabase() {
-    const transaction =db.transaction(["pending"], "readWrite");
-    const store = transaction.objectStore("pending");
-    store.add(record)
-    
-    getAll.onsuccess = function(){
-        if (getAll.result.length >0) {
-            fetch("/api/transaction/bulk", {
-                method: "POST",
+function uploadTransaction() {
+
+    // open a transaction on your db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+
+        // if there was data in indexedDb's store send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
-                    Accept: "application/json, text/plain, */*",
-                    "Content-Type": "application/json"
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
                 }
             })
-            .then(response => response.json())
-            .then(() => {
-                const transaction = db.transaction(["pending"], "readwrite");
-                const store = db.transaction.objectStore(["pending"])
-                store.clear()
-            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    // open one more transaction
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+                    // access the new_transaction object store
+                    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+                    // clear all items in your store
+                    budgetObjectStore.clear();
+
+                    alert('All saved transactions has been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
     }
 }
 
-window.addEventListener("online", checkDatabase);
-//the ability to enter expenses offline.
-
-//offline entries should be added to the tracker when the application is bought back online
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
